dd924592461a45d8bee8232b902b365c47fe04bb
MISIGNE <lilboythomson@gmail.com>
2020-01-16 11:18:20 +0100

    Adjusting example application

--- lib/neuomorphic_container.dart
+++ lib/neuomorphic_container.dart
@@ -4,7 +4,7 @@ import 'package:flutter/material.dart';
 
 /// A Neuomorphic Container.
 // ignore: camel_case_types
-class neuomorphic_container extends StatelessWidget {
+class NeuomorphicContainer extends StatelessWidget {
   final Key key;
   final AlignmentGeometry alignment;
   final EdgeInsetsGeometry padding;
@@ -18,9 +18,24 @@ class neuomorphic_container extends StatelessWidget {
   final Widget child;
   final BorderRadiusGeometry borderRadius;
   final BoxShape shape;
+  final BoxBorder border;
 
-  neuomorphic_container({this.key,this.child,this.alignment,this.padding,this.foregroundDecoration,this.width,this.height,this.shape,this.constraints,this.borderRadius,this.clipBehavior=Clip.none,this.margin,this.transform}):
-        assert(margin == null || margin.isNonNegative),
+  NeuomorphicContainer(
+      {this.key,
+      this.child,
+      this.alignment,
+      this.padding,
+      this.foregroundDecoration,
+      this.width,
+      this.height,
+      this.shape = BoxShape.rectangle,
+      this.constraints,
+      this.borderRadius,
+      this.clipBehavior = Clip.none,
+      this.margin,
+      this.transform,
+      this.border})
+      : assert(margin == null || margin.isNonNegative),
         assert(padding == null || padding.isNonNegative),
         assert(constraints == null || constraints.debugAssertIsValid()),
         assert(clipBehavior != null),
@@ -29,12 +44,32 @@ class neuomorphic_container extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      key: key,alignment: alignment,padding: padding,foregroundDecoration: foregroundDecoration,width: width,height: height,constraints: constraints,margin:margin,transform: transform,clipBehavior: clipBehavior,child:child,
-      decoration: BoxDecoration(boxShadow: [
-        BoxShadow(color: Color.fromRGBO(209, 205, 199, 1.0),offset: Offset(6.0,6.0),blurRadius: 16.0),
-        BoxShadow(color: Color.fromRGBO(255, 255, 255, 1.0),offset: Offset(-6.0,-6.0),blurRadius: 16.0),
-      ],color: Color.fromRGBO(239, 238, 238, 1.0),borderRadius: borderRadius,shape: shape),
+      key: key,
+      alignment: alignment,
+      padding: padding,
+      foregroundDecoration: foregroundDecoration,
+      width: width,
+      height: height,
+      constraints: constraints,
+      margin: margin,
+      transform: transform,
+      clipBehavior: clipBehavior,
+      child: child,
+      decoration: BoxDecoration(
+          boxShadow: [
+            BoxShadow(
+                color: Color.fromRGBO(209, 205, 199, 1.0),
+                offset: Offset(6.0, 6.0),
+                blurRadius: 16.0),
+            BoxShadow(
+                color: Color.fromRGBO(255, 255, 255, 1.0),
+                offset: Offset(-6.0, -6.0),
+                blurRadius: 16.0),
+          ],
+          color: Color.fromRGBO(239, 238, 238, 1.0),
+          borderRadius: borderRadius,
+          shape: shape,
+          border: border),
     );
   }
 }
-
